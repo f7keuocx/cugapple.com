3e4ef4a1eea192ee07ead95d8244beddf230f6ed
f7keuocx <f7keuocx@qq.com>
2014-02-26 20:01:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+(function($){
+	
+	var $priceArea = $('#priceArea');
+
+	$.getJSON('/price.json').done(function(data){
+
+		var html = '<ul>';
+
+		for(var product in data){
+			if(product.hasOwnProperty(product)){
+				for(var model in product){
+					if(product.hasOwnProperty(model) && product[model]){
+						html += '<li>' + model + ':￥' + product[model] + '</li>';
+					}
+				}
+			}
+		}
+		html += '</ul>';
+
+		$priceArea.html(html);
+
+	}).fail(function(){
+
+		$priceArea.html('获取价格失败，请联系店主。');
+
+	});
+
+})(jQuery);
