e0dd93dec5c1088083677fa7b3591b4af63fcabc
TooBug <i@toobug.net>
2014-03-12 01:37:23 -0400

    修改页面样式，加入报价更新时间

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 $(function(){	
 	var $priceArea = $('#priceArea');
 
-	$.getJSON('/price.json').done(function(data){
+	$.getJSON('./price.json').done(function(data,msg,xhr){
+
+        var updateTime = moment(xhr.getResponseHeader("Last-Modified"));
+        
+        $('#updateTime .time').html(updateTime.format('MM-DD HH:mm'));
 
 		var html = '<ul>';
 
 		for(var product in data){
 			if(data.hasOwnProperty(product)){
+				html += '<li class="product"><h3>' + product + '</h3><ul class="modelList">';
 				for(var model in data[product]){
 					if(data[product].hasOwnProperty(model) && data[product][model]){
-						html += '<li><em>' + product + '</em> ' +  model + ':￥' + data[product][model] + '</li>';
+						html += '<li class="model">' + formatColor(model) + ':<span class="price">￥' + data[product][model] + '</span></li>';
 					}
 				}
+				html += '</ul></li>'
 			}
 		}
 		html += '</ul>';
@@ -24,6 +30,16 @@ $(function(){
 		$priceArea.html('获取价格失败，请联系店主。');
 
 	});
+	
+	function formatColor(str){
+	    return str.replace(/white/gi,'白色')
+	            .replace(/black/gi,'黑色')
+	            .replace(/golden/gi,'金色')
+	            .replace(/blue/gi,'蓝色')
+	            .replace(/yellow/gi,'黄色')
+	            .replace(/pink/gi,'粉色')
+	            .replace(/green/gi,'绿色');
+	}
 
 });
 })(jQuery);
